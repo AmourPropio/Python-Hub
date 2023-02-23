4ab59b70f3ea6f6c647dfc3d4d386dba7722635a
AmourPropio <125149853+AmourPropio@users.noreply.github.com>
2023-02-23 12:36:31 +0530

    added

--- CommerceProject/EcomProject/urls.py
+++ CommerceProject/EcomProject/urls.py
@@ -1,4 +1,4 @@
-"""CommerceProject URL Configuration
+"""EcomProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
--- CommerceProject/EcomProject/wsgi.py
+++ CommerceProject/EcomProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for CommerceProject project.
+WSGI config for EcomProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CommerceProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EcomProject.settings')
 
 application = get_wsgi_application()
